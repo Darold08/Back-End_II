c4d1c196c4fd066119bb4309841be97aca85ccc9
Darold08 <liviaaadarold20@gmail.com>
2025-01-31 11:24:33 -0400

    time

--- api_libertadores/index.js
+++ api_libertadores/index.js
@@ -1,13 +1,40 @@
 import express from 'express';
-import { retornaCampeonatos, retornaCampeonatosID} from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatos, retornaCampeonatosAno, retornaCampeonatosID, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
 // import pool from './servico/conexao.js';
 
 const app = express();
 
 app.get('/campeonatos', async (req, res) => {
-    const campeonatos = await retornaCampeonatos();
-    res.json(campeonatos);
+    let campeonatos;
+    const ano = req.query.ano;
+    const time = req.query.time;
+    
+    if (typeof ano === 'undefined' && typeof time === 'undefined') {
+        campeonatos = await retornaCampeonatos();
+    } 
+    else if (typeof ano !== 'undefined') {
+        campeonatos = await retornaCampeonatosAno(ano)
+    }
+    else if (typeof time !== 'undefined') {
+        campeonatos = await retornaCampeonatosTime(time);
+    } 
+    if (campeonatos.length > 0) {
+        res.json(campeonatos);
+    } else {
+        res.status(404).json({ mensagem: "Nenhum campeonato encontrado"});
+    } 
 })
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+    if(campeonato.length > 0) {
+        res.json(campeonato);
+    } else {
+        res.status(404).json({ mensagem: "Nenhum campeonato encontrado"})
+    }
+});
+
 
 app.listen(9000, () => {
     const data = new Date();
@@ -20,10 +47,6 @@ app.listen(9000, () => {
     // conexao.release();
 })
 
-app.get('/campeonatos/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
-    const campeonato = await retornaCampeonatosID(id);
-    res.json(campeonato);
-})
+
 
 

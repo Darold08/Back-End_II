8445c159a0ba16b969ab76fdb532d15d0f089533
Darold08 <liviaaadarold20@gmail.com>
2025-02-17 07:47:07 -0400

    leads

--- api_libertadores/index.js
+++ api_libertadores/index.js
@@ -3,11 +3,23 @@ import express from 'express';
 import { retornaCampeonatos, retornaCampeonatosAno, retornaCampeonatosID, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
 import { cadastraCampeonato } from './servico/cadastroCampeonato_servico.js';
 import { atualizaCampeonato, atualizaCampeonatoParcial } from './servico/atualizaCampeonato_servico.js';
+import { deletaCampeonato } from './servico/deletaCampeonatos_servico.js';
 // import pool from './servico/conexao.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json()); //suporte para JSON no corpo da requisiçao, para não dar erro
+
+app.delete('/campeonatos/:id', async (req, res) =>{
+    const {id} = req.params;
+    const resultado = await deletaCampeonato(id);
+
+    if (resultado.affectedRows > 0) {
+        res.status(202).send('Registro deletado com sucesso');
+    } else {
+        res.status(404).send('Registro não encontrado');
+    }
+})
 
 app.patch('/campeonatos/:id', async(req, res) => {
     const {id} = req.params;
